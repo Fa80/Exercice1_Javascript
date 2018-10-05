d9e952483bc49bc46d1add9326e6a977043b47a8
Fa <ngomfa@gmail.com>
2018-10-05 15:32:06 +0200

    exo1

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+/*
+Déclaration de la variable name
+initialisé avec chaine de caractére (string)
+*/
+var name = "Fa";
+
+/*
+affichage de ma variable name dans une boite de dialogue que j'appelle avec la foncion alert
+*/
+
+  alert (name);
